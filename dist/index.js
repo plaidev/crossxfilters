d0f60473e9b4ff3c87576f8343a243f16736aa2f
Naoto Kato <naoto.kato@plaid.co.jp>
2017-09-25 12:49:31 +0900

    [build] update build files

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('babel-runtime/helpers/classCallCheck'), require('babel-runtime/helpers/createClass')) :
+	typeof define === 'function' && define.amd ? define(['exports', 'babel-runtime/helpers/classCallCheck', 'babel-runtime/helpers/createClass'], factory) :
+	(factory((global.Cross2Filters = {}),global._classCallCheck,global._createClass));
+}(this, (function (exports,_classCallCheck,_createClass) { 'use strict';
+
+_classCallCheck = _classCallCheck && _classCallCheck.hasOwnProperty('default') ? _classCallCheck['default'] : _classCallCheck;
+_createClass = _createClass && _createClass.hasOwnProperty('default') ? _createClass['default'] : _createClass;
+
+var Manager = function () {
+  function Manager() {
+    _classCallCheck(this, Manager);
+
+    this;
+  }
+
+  _createClass(Manager, [{
+    key: "registerDataset",
+    value: function registerDataset() {}
+  }]);
+
+  return Manager;
+}();
+
+exports.Manager = Manager;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=index.js.map
---
+++ dist/index.mjs
@@ -0,0 +1,10 @@
+class Manager {
+  constructor() {
+    this;
+  }
+
+  registerDataset() {}
+}
+
+export { Manager };
+//# sourceMappingURL=index.mjs.map
